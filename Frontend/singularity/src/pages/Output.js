d63c2f8737dd2275b2508c4ddc49901e40b103bb
kartikeydwivedi <kartikey.dwivedi@nagarro.com>
2023-03-26 16:35:30 +0530

    done

--- Frontend/singularity/src/pages/Output.js
+++ Frontend/singularity/src/pages/Output.js
@@ -3,24 +3,24 @@ import { useLocation } from 'react-router'
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
 const Output = ( props ) => {
     const location = useLocation();
-    const data = location.state;
-    console.log( data )
+    const state = location.state;
+    console.log( state );
     return (
         <div className="container-fluid " style={ { backgroundColor: 'black', height: '100vh' } } >
-            <div class="row p-5" style={ { marginBottom: '477px' } }>
-                <div class="col-md-3"></div>
-                <div class="col-md-6">
-                    {state !=0 &&<div class="card card-body alert alert-danger"><center>Sorry! Please consult DOCTOR.</center></div>
+            <div className="row p-5" style={ { marginBottom: '477px' } }>
+                <div className="col-md-3"></div>
+                <div className="col-md-6">
+                    {state !=0 &&<div className="card card-body alert alert-danger"><center>Sorry! Please consult DOCTOR.</center></div>
 }
-                   {state==0 && <div class="card card-body alert alert-success"><center>Great! You are HEALTHY.</center></div>
+                   {state==0 && <div className="card card-body alert alert-success"><center>Great! You are HEALTHY.</center></div>
 }
-                    <div class="row">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-4"><Link to="/"><a class="btn btn-block btn-primary">Back to Home</a></Link></div>
-                        <div class="col-md-4"></div>
+                    <div className="row">
+                        <div className="col-md-4"></div>
+                        <div className="col-md-4"><Link to="/"><a className="btn btn-block btn-primary">Back to Home</a></Link></div>
+                        <div className="col-md-4"></div>
                     </div>
                 </div>
-                <div class="col-md-3"></div>
+                <div className="col-md-3"></div>
             </div>
         </div>
     )
